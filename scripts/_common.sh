d996d9dd6519096a280bf32678db1bb000bc5b9d
yalh76 <yalh@yahoo.com>
2022-10-16 00:09:30 +0200

    Fix removal

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,7 +9,7 @@ pkg_dependencies=""
 
 demo_repository="https://github.com/YunoHost/yunohost_demo"
 
-demo_release="c3dee09570832ceffa8e92221ed0b87fb29b8500"
+demo_release="ed5976b72219683de681fa0f532d09367003dfdc"
 
 app_dependencies="lxd"
 
