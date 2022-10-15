67b5fbf7fec207c413e10876fc6d097e818efcdf
yalh76 <yalh@yahoo.com>
2022-10-15 23:37:39 +0200

    Only private

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,7 +9,7 @@ pkg_dependencies=""
 
 demo_repository="https://github.com/YunoHost/yunohost_demo"
 
-demo_release="af5d9e4f3702e7a4d5da4cb560b37a93da6e1a0e"
+demo_release="c3dee09570832ceffa8e92221ed0b87fb29b8500"
 
 app_dependencies="lxd"
 
